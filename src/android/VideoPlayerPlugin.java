f84f64b0948db3a185daccbc383bbae4babea9f9
phi <phi1618jp@gmail.com>
2016-03-29 17:24:42 +0900

    fix video player

--- src/android/VideoPlayerPlugin.java
+++ src/android/VideoPlayerPlugin.java
@@ -20,24 +20,22 @@ import android.content.DialogInterface.OnCancelListener;
 import android.content.DialogInterface.OnDismissListener;
 
 import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnCompletionListener;
-import android.media.MediaPlayer.OnErrorListener;
-import android.media.MediaPlayer.OnPreparedListener;
 
 import android.view.Gravity;
 import android.view.SurfaceHolder;
+import android.view.Display;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.VideoView;
 
-public class VideoPlayerPlugin extends CordovaPlugin implements OnCompletionListener, OnPreparedListener, OnErrorListener, OnDismissListener {
+public class VideoPlayerPlugin extends CordovaPlugin {
+
   private CallbackContext callbackContext = null;
 
-  private Dialog dialog;
+  private Dialog dialog = null;
   private VideoView videoView;
-  private MediaPlayer player;
 
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
     super.initialize(cordova, webView);
@@ -49,8 +47,8 @@ public class VideoPlayerPlugin extends CordovaPlugin implements OnCompletionList
     if (action.equals("show")) {
       show(action, args, callbackContext);
       return true;
-    } else if (action.equals("destroy")){
-      // showAds(action, args, callbackContext);
+    } else if (action.equals("destory")){
+      destory(action, args, callbackContext);
       return true;
     }
 
@@ -58,18 +56,27 @@ public class VideoPlayerPlugin extends CordovaPlugin implements OnCompletionList
     return false;
   }
 
-  private void show(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+  private void show(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+    if (dialog != null) {
+      dialog.dismiss();
+      dialog = null;
+    }
+
     final String urlString = args.getString(0);
+    final JSONArray rect = args.getJSONArray(1);
+    int x = rect.getInt(0);
+    int y = rect.getInt(1);
+    int width = rect.getInt(2);
+    int height = rect.getInt(3);
     String path = urlString;
+    Uri uri = Uri.parse(path);
 
     this.callbackContext = callbackContext;
 
     // dialog
     dialog = new Dialog(cordova.getActivity(), android.R.style.Theme_NoTitleBar);
     dialog.getWindow().getAttributes().windowAnimations = android.R.style.Animation_Dialog;
-    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-    dialog.setCancelable(true);
-    dialog.setOnDismissListener(this);
+//    dialog.setOnDismissListener(this);
 
     // Main container layout
     LinearLayout main = new LinearLayout(cordova.getActivity());
@@ -80,92 +87,37 @@ public class VideoPlayerPlugin extends CordovaPlugin implements OnCompletionList
 
     videoView = new VideoView(cordova.getActivity());
     videoView.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-//    videoView.setVideoPath(path);
-    videoView.setVideoURI(Uri.parse(path));
+    videoView.setVideoURI(uri);
     videoView.start();
     main.addView(videoView);
 
-    player = new MediaPlayer();
-    player.setOnPreparedListener(this);
-    player.setOnCompletionListener(this);
-    player.setOnErrorListener(this);
-
-
-    try {
-      player.setDataSource(path);
-    } catch(Exception e) {
-      return ;
-    }
-
-    final SurfaceHolder mHolder = videoView.getHolder();
-    mHolder.setKeepScreenOn(true);
-    mHolder.addCallback(new SurfaceHolder.Callback() {
-      @Override
-      public void surfaceCreated(SurfaceHolder holder) {
-        player.setDisplay(holder);
-        try {
-          player.prepare();
-        } catch (Exception e) {
-          PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getLocalizedMessage());
-          result.setKeepCallback(false); // release status callback in JS side
-//          callbackContext.sendPluginResult(result);
-//          callbackContext = null;
-        }
-      }
-
-      @Override
-      public void surfaceDestroyed(SurfaceHolder holder) {
-        player.release();
-      }
-
-      @Override
-      public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-      }
-    });
-
+    WindowManager wm = cordova.getActivity().getWindowManager();
+    Display disp = wm.getDefaultDisplay();
     WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
-    lp.copyFrom(dialog.getWindow().getAttributes());
-    lp.width = WindowManager.LayoutParams.MATCH_PARENT;
-    lp.height = WindowManager.LayoutParams.MATCH_PARENT;
+    lp.x = x - disp.getWidth()/2;
+    lp.y = y - disp.getHeight()/2;
+    lp.width = width;
+    lp.height = height;
 
     dialog.setContentView(main);
     dialog.show();
     dialog.getWindow().setAttributes(lp);
-//    cordova.getActivity().setContentView(main);
+
+    // On Completion
+    videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+      @Override
+      public void onCompletion(MediaPlayer mediaplayer) {
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Done"));
+        dialog.dismiss();
+        dialog = null;
+      }
+    });
   }
 
-
-  @Override
-  public boolean onError(MediaPlayer mp, int what, int extra) {
-    // Log.e(LOG_TAG, "MediaPlayer.onError(" + what + ", " + extra + ")");
-    // if(mp.isPlaying()) {
-    //   mp.stop();
-    // }
-    // mp.release();
-    // dialog.dismiss();
-     return false;
-  }
-
-  @Override
-  public void onPrepared(MediaPlayer mp) {
-     mp.start();
-  }
-
-  @Override
-  public void onCompletion(MediaPlayer mp) {
-//     Log.d("MediaPlayer completed");
-     mp.release();
-     dialog.dismiss();
-  }
-
-  @Override
-  public void onDismiss(DialogInterface dialog) {
-    // Log.d(LOG_TAG, "Dialog dismissed");
-    // if (callbackContext != null) {
-    //   PluginResult result = new PluginResult(PluginResult.Status.OK);
-    //   result.setKeepCallback(false); // release status callback in JS side
-    //   callbackContext.sendPluginResult(result);
-    //   callbackContext = null;
-    // }
+  private void destory(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+    if (dialog != null) {
+      dialog.dismiss();
+      dialog = null;
+    }
   }
 }
